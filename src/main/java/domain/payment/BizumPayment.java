dab38cbd751ca305cf91a75955693430b1627f27
shundong1 <126656305+shundong1@users.noreply.github.com>
2025-04-02 13:19:03 +0200

    Payment

--- src/main/java/domain/payment/BizumPayment.java
+++ src/main/java/domain/payment/BizumPayment.java
@@ -1,4 +1,23 @@
 package domain.payment;
 
 public class BizumPayment extends Payment{
+    @Override
+    protected void obtainPaymentData() {
+
+    }
+
+    @Override
+    protected void validateData() {
+
+    }
+
+    @Override
+    protected void executePayment() {
+
+    }
+
+    @Override
+    protected void sendReceipt() {
+
+    }
 }
--- src/main/java/domain/payment/Payment.java
+++ src/main/java/domain/payment/Payment.java
@@ -4,7 +4,31 @@ import domain.order.ConfirmedOrder;
 
 import java.time.LocalDate;
 
-public abstract class Payment extends ConfirmedOrder {
-    public LocalDate getStart() {
+public abstract class Payment  { //extends ConfirmedOrder {
+    private LocalDate start;
+    private LocalDate end;
+
+    public Payment(){
+        this.start = LocalDate.now();
     }
+    public void processPayment() {
+        obtainPaymentData();
+        validateData();
+        executePayment();
+        setEndDate();
+        sendReceipt();
+    }
+    public LocalDate getStart() {
+        return start;
+    }
+    public LocalDate getEnd() {
+        return end;
+    }
+    protected void setEndDate() {
+        this.end = LocalDate.now();
+    }
+    protected abstract void obtainPaymentData();
+    protected abstract void validateData();
+    protected abstract void executePayment();
+    protected abstract void sendReceipt();
 }
